e51eee13dd7a39c3d5766dd6356dc75197f39037
pengruinan <18720709772@163.com>
2018-05-18 14:32:43 +0800

    add uploadFile components

--- src/components/uploadFile/index.vue
+++ src/components/uploadFile/index.vue
@@ -7,7 +7,7 @@
             </div>
         </Upload>
         <div class="default-list" v-for="(item, index) in defaultList" :key="index">
-            <p class="list-item" v-if="item.status === 'finished'">
+            <p class="list-item">
                 <Icon type="document-text"></Icon>
                 <span>{{item.name}}</span>
                 <Icon @click.native="onRemove(index)" class="delete" type="trash-a"></Icon>
@@ -21,11 +21,11 @@
 <script>
     export default {
         // /uploadFile
-        props: ["uploadUrl"],
+        props: ["uploadUrl", 'model'],
         data() {
             return {
                 baseUrl: BASE_URL,
-                defaultList:[],
+                defaultList: [],
                 percentage: 0
             }
         },
@@ -35,61 +35,97 @@
 
             },
             // 上传时的钩子
-            onUploading(){
+            onUploading() {
                 var timer = setInterval(() => {
                     if (this.percentage <= 100) {
-                        this.percentage += Math.random()*5+1;
-                    }else{
+                        this.percentage += Math.random() * 5 + 1;
+                    } else {
                         this.percentage = 100;
                         clearInterval(timer);
                     }
                 }, 10)
             },
             // 文件上传成功时的钩子
-            onSuccess(res, file, fileList){
+            onSuccess(res, file, fileList) {
                 if (res.code == 200) {
                     this.defaultList = fileList;
+                    this.$emit("update:model", res.data.path)
                     this.percentage = 0;
-                }else{
+                } else {
                     this.$Message.error(file.name + ' 上传失败！');
                 }
-
             },
-            onRemove(item){
-                console.log(item);
+            onRemove(item) {
+                this.defaultList.splice(item, 1);
             }
         },
-        mounted(){
-
+        watch: {
+            model(newVal, oldVal) {
+                this.defaultList = [];
+                if (newVal) {
+                    // this.fileName = this.model;
+                    let obj = {
+                        name: newVal,
+                        path: newVal
+                    }
+                    this.defaultList.push(obj)
+                }
+            }
+        },
+        mounted() {
+            if (this.model) {
+                let obj = {
+                    name: this.model,
+                    path: this.model
+                }
+                this.defaultList.push(obj)
+            }
         }
     }
 </script>
 
 <style scoped>
     .container {
-        width: 50%;
+        width: 100%;
     }
-    .default-list{
+
+    .default-list {
         padding: 5px;
     }
-    .list-item{
+
+    .list-item {
         font-size: 14px;
         cursor: pointer;
         height: 25px;
         line-height: 25px;
         border: 1px solid #ccc;
         padding-left: 5px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
     }
-    .list-item:hover{
+
+    .list-item span {
+        display: inline-block;
+        width: 80%;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+
+    .list-item:hover {
         border: 1px solid blueviolet;
     }
-    .list-item:hover span{
+
+    .list-item:hover span {
         color: blueviolet;
     }
-    .list-item i{
+
+    .list-item i {
         margin-right: 5px;
     }
-    .delete{
+
+    .delete {
         float: right;
         color: rgb(196, 36, 36);
         line-height: 25px;
--- src/views/demo-table/index.vue
+++ src/views/demo-table/index.vue
@@ -1,18 +1,154 @@
-<template>
+<template lang="html">
     <div class="container">
-        <UploadFile :uploadUrl="'uploadFile'"></UploadFile>
+        <BaseTable ref="table" :cols="cols" @beforeCommit="beforeCommit" @handleAdd="handleAdd" :searchData="searchInfo" :api='api' :editInfo="editInfo">
+            <!-- 搜索 -->
+            <div slot="searchBox" class="search-box">
+                <Input v-model="searchInfo.id" icon="search" placeholder="输入搜素条件..." style="width: 200px"></Input>
+            </div>
+            <div slot="searchMoreBox" class="search-box">
+                <Input v-model="searchInfo.b" icon="search" placeholder="输入搜素条件..." style="width: 200px"></Input>
+                <Input v-model="searchInfo.a" icon="search" placeholder="输入搜素条件..." style="width: 200px"></Input>
+                <Input v-model="searchInfo.c" icon="search" placeholder="输入搜素条件..." style="width: 200px"></Input>
+            </div>
+            <div slot="dialog">
+                <Form :model="editInfo" ref="formCustom" :rules="ruleCustom" label-position="right" :label-width="100">
+                    <FormItem label="姓名" prop="name">
+                        <Input v-model="editInfo.name"></Input>
+                    </FormItem>
+                    <FormItem label="头像">
+                        <UploadFile :model.sync="editInfo.avatar" :uploadUrl="'uploadFile'"></UploadFile>
+                    </FormItem>
+                </Form>
+            </div>
+        </BaseTable>
     </div>
 </template>
 
 <script>
+    import BaseTable from '@/components/baseTable';
     import UploadFile from '@/components/uploadFile'
     export default {
-        components:{UploadFile}
+        components: {
+            BaseTable, UploadFile
+        },
+        data() {
+            return {
+                searchInfo: {},
+                editInfo: {},
+                baseUrl: BASE_URL,
+                api: {
+                    save: '/enrollInfo/save',
+                    remove: '/enrollInfo/remove',
+                    update: '/enrollInfo/update',
+                    search: '/enrollInfo/search',
+                    list: '/enrollInfo/list'
+                },
+                // 表单验证
+                ruleCustom: {
+                    name: [
+                        { required: true, message: '不能为空', trigger: 'blur' }
+                    ],
+                    answer: [
+                        { required: true, message: '不能为空', trigger: 'blur' }
+                    ],
+                },
+                cols: [{
+                    type: 'selection',
+                    width: 60,
+                    align: 'center'
+                }, {
+                    title: 'ID',
+                    key: 'id'
+                }, {
+                    title: '姓名',
+                    key: 'name'
+                }, {
+                    title: '头像',
+                    key: 'avatar',
+                    render: (h, params) => {
+                        return h('div',[
+                            h('img', {
+                                attrs:{
+                                    'src': this.baseUrl + params.row.avatar
+                                }
+                            })
+                        ])
+                    }
+                }, {
+                    title: '操作',
+                    key: 'action',
+                    width: 150,
+                    align: 'center',
+                    render: (h, params) => {
+                        return h('div', [
+                            h('Button', {
+                                props: {
+                                    type: 'primary',
+                                    size: 'small'
+                                },
+                                style: {
+                                    marginRight: '5px'
+                                },
+                                on: {
+                                    click: () => {
+                                        this.handleEdit(params)
+                                    }
+                                }
+                            }, '编辑'),
+                            h('Button', {
+                                props: {
+                                    type: 'error',
+                                    size: 'small'
+                                },
+                                on: {
+                                    click: () => {
+                                        this.handleRemove(params)
+                                    }
+                                }
+                            }, '删除')
+                        ]);
+                    }
+                }]
+            }
+        },
+        methods: {
+            beforeCommit(param) {
+                this.$refs.formCustom.validate((valid) => {
+                    if (valid) {
+                        param.ok = true; // 允许提交
+                    } else {
+                        param.ok = false; // 允许提交
+                    }
+                });
+
+            },
+            handleEdit(param) {
+                this.editInfo = JSON.parse(JSON.stringify(param.row));
+                let params = {
+                    needNew: false
+                }
+                this.$refs.table.handleEdit(params)
+            },
+            handleAdd() {
+                let params = {
+                    needNew : false
+                } //不打开新页面
+                this.editInfo = {};
+                this.$refs.table.afterAdd(params)
+            },
+            handleRemove(params) {
+                this.$refs.table.handleRemove(params, 'one')
+            }
+        },
     }
 </script>
 
-<style scoped>
-    .container{
-        width: 600px;
+<style lang="less" scoped>
+    .search-box {
+        width: auto;
+        float: left;
     }
 </style>
+
+
+
